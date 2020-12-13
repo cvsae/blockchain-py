598ff1f30a1a5e804dafa774985ca90c99d0c69b
cvsae <33999023+cvsae@users.noreply.github.com>
2020-12-13 17:17:31 +0200

    Update transaction_input.py
    vin setter's

--- blockchain-py/transaction_input.py
+++ blockchain-py/transaction_input.py
@@ -17,11 +17,17 @@ class TXInput(object):
         _public_key (string): Public key.
     """
 
-    def __init__(self, txid, vout, sig, pubkey):
-        self._tx_id = utils.encode(txid)
-        self._vout = vout
-        self._sig = sig
-        self._public_key = pubkey
+    def __init__(self, txid = None, vout= None, sig = None, pubkey = None):
+        self._tx_id = None
+        self._vout = None
+        self._sig = None
+        self._public_key = None
+        
+        if txid != None and vout != None and sig != None and pubkey != None:
+            self._tx_id = utils.encode(txid)
+            self._vout = vout
+            self._sig = sig
+            self._public_key = pubkey
 
     def uses_key(self, pubkey_hash):
         # checks whether the address initiated the transaction
@@ -29,20 +35,31 @@ class TXInput(object):
         return pubkey_hash == pubkey_hash
 
     def __repr__(self):
-        return 'TXInput(tx_id={0!r}, vout={1!r}, signature={2!r}, public_key={3!r})'.format(
-            self._tx_id, self._vout, self._sig, self._public_key)
+        return 'TXInput(tx_id={0!r}, vout={1!r}, signature={2!r}, public_key={3!r})'.format(self._tx_id, self._vout, self._sig, self._public_key)
 
     @property
     def tx_id(self):
         return utils.decode(self._tx_id)
 
+    @tx_id.setter
+    def tx_id(self, r_value):
+        self._tx_id = utils.encode(r_value)
+
     @property
     def vout(self):
         return self._vout
 
+    @vout.setter
+    def vout(self, r_value):
+        self._vout = r_value
+
     @property
     def signature(self):
         return self._sig
+
+    @signature.setter
+    def signature(self, r_value):
+        self._sig = r_value
 
     @property
     def public_key(self):
